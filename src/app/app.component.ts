1b153ab80ecc823cacafd5f9093d2fa25a256ef0
Jorge Barrón <jorgeb8168711@gmail.com>
2019-04-23 16:48:44 -0500

    Pwa configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpMethod } from 'blocking-proxy/built/lib/webdriver_commands';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Data, Result } from './interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-pwa';
+export class AppComponent implements OnInit {
+  public data = [];
+  public loading = false;
+  private baseUrl = 'https://gateway.marvel.com:443/v1/public/';
+  private path = 'characters';
+  private key = '35591db06a438788c7c8fcaab330df1a';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.httpClient.get<Response>(`${this.baseUrl}/characters?apikey=${this.key}`).toPromise()
+      .then((res: Response) => {
+        this.data = res['data'];
+        console.log(res, this.data);
+        this.loading = false;
+      })
+      .catch(err => console.log(err));
+  }
+
+  public getImageUrl(item: Result) {
+    return `${item.thumbnail.path}.${item.thumbnail.extension}`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
